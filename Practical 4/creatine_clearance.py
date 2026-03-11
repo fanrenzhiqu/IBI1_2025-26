51867c2c2c3d2d7d0cd9c6bb59d7f0c1d7409de0
Zhenjia Gong <zhenjia.25@intl.zju.edu.cn>
2026-03-11 17:52:49 +0800

    Practical 4

--- Practical 4/creatine_clearance.py
+++ Practical 4/creatine_clearance.py
@@ -1,29 +1,26 @@
-# Ask the user to enter age, weight, gender and creatine concentration
 age = int(input("Enter age (years): "))
-weight = float(input("Enter weight (kg): "))
-gender = input("Enter gender (male/female): ")
-cr = float(input("Enter creatine concentration (µmol/L): "))
-
-# Check whether the input values are valid
 if age >= 100:
     print("Age needs corrected")
+    exit()
 
-elif weight <= 20 or weight >= 80:
+weight = float(input("Enter weight (kg): "))
+if weight <= 20 or weight >= 80:
     print("Weight needs corrected")
+    exit()
 
-elif cr <= 0 or cr >= 100:
-    print("Creatine concentration needs corrected")
-
-elif gender != "male" and gender != "female":
+gender = input("Enter gender (male/female): ").lower()
+if gender != "male" and gender != "female":
     print("Gender needs corrected")
+    exit()
 
-else:
-    # Calculate creatine clearance
-    crcl = ((140 - age) * weight) / (72 * cr)
+cr = float(input("Enter creatine concentration (µmol/L): "))
+if cr <= 0 or cr >= 100:
+    print("Creatine concentration needs corrected")
+    exit()
 
-    # Adjust for female
-    if gender == "female":
-        crcl = crcl * 0.85
+crcl = ((140 - age) * weight) / (72 * cr)
 
-    # Print result
-    print("Creatine clearance rate:", crcl)
+if gender == "female":
+    crcl = crcl * 0.85
+
+print("Creatine clearance rate:", crcl)
