fe9d8c888a7f0d2f88db3996e18fca3b1dc76a30
Zhenjia Gong <zhenjia.25@intl.zju.edu.cn>
2026-03-11 19:44:29 +0800

    Practical 4

---
+++ Practical 4/variables.py
@@ -0,0 +1,42 @@
+# 4.1 Some simple math
+
+a = 5.08
+b = 5.33
+c = 5.55
+
+# population change
+d = b - a
+e = c - b
+
+print(d)
+print(e)
+
+# Compare growth rates
+if e > d:
+    print("Population growth is accelerating.")
+elif e < d:
+    print("Population growth is decelerating.")
+else:
+    print("Population growth is constant.")
+
+# Comment: d = 0.25 and e = 0.22, so the population growth is decelerating.
+
+
+
+
+# 4.2 Booleans
+
+X = True
+Y = False
+
+W = X or Y
+
+print(W)
+
+# Truth table for OR
+# X     Y     X or Y
+# True  True  True
+# True  False True
+# False True  True
+# False False False
+
