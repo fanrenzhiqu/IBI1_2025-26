27f2c92b578d6a9c5c77e2ff0b2722bfef7dbca3
Zhenjia Gong <zhenjia.25@intl.zju.edu.cn>
2026-05-08 22:02:11 +0800

    Fix input checks in creatinine code

--- Practical 4/creatine_clearance.py
+++ Practical 4/creatine_clearance.py
@@ -11,24 +11,25 @@
 # If gender is female, multiply the result by 0.85
 # Print the creatine clearance rate
 
+
 age = int(input("Enter age (years): "))
 if age >= 100:
-    print("Age needs corrected")
+    print("Age needs corrected!")
     exit()
 
 weight = float(input("Enter weight (kg): "))
 if weight <= 20 or weight >= 80:
-    print("Weight needs corrected")
+    print("Weight needs corrected!")
     exit()
 
 gender = input("Enter gender (male/female): ").lower()
 if gender != "male" and gender != "female":
-    print("Gender needs corrected")
+    print("Gender needs corrected!")
     exit()
 
 cr = float(input("Enter creatine concentration (µmol/L): "))
 if cr <= 0 or cr >= 100:
-    print("Creatine concentration needs corrected")
+    print("Creatine concentration needs corrected!")
     exit()
 
 crcl = ((140 - age) * weight) / (72 * cr)
